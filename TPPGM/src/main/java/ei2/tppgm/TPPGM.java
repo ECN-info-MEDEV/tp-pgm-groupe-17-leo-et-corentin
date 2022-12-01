88e6e0378295f1f62e68d622daea5bd5d44c763d
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-12-01 10:50:33 +0100

    lecture n'est pas fini, mais Elon Musk menace de me virer parce que mes stats GitHub ne sont pas suffisantes, donc je fais ça. Normalement ça devrait être bon au prochain commit.

--- TPPGM/src/main/java/ei2/tppgm/TPPGM.java
+++ TPPGM/src/main/java/ei2/tppgm/TPPGM.java
@@ -5,7 +5,11 @@
 
 package ei2.tppgm;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import static java.lang.Integer.parseInt;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -14,10 +18,38 @@ import java.util.ArrayList;
 public class TPPGM {
     private String source;
 
+   /**
+    * Constructeur de TPPGM 
+    * @param source : Source du fichier 
+    */
     public TPPGM(String source) {
         this.source = source;
     }
-    
+    /**
+     * Fonction de lecture d'un fichier PGM
+     */
+    private void lecture(){
+        try{
+            String ligne;
+            BufferedReader fichier = new BufferedReader(new FileReader(source));
+            StringTokenizer tokenizer = new StringTokenizer(ligne, " ");
+            ligne = fichier.readLine();
+            ligne = fichier.readLine();
+            ligne = fichier.readLine();
+            int x = parseInt(tokenizer.nextToken());
+            int y = parseInt(tokenizer.nextToken());
+            
+            ligne = fichier.readLine();
+            ligne = fichier.readLine();
+            while (ligne != null){
+                
+                ligne = fichier.readLine();
+            }
+            fichier.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     
     public static void seuillage(ArrayList<Integer> mat, int seuil){
         
